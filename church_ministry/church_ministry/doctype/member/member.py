9d3218b87625ed1a7eb2b05ee90580302bfa92ee
Gangadhar Kadam <gangadhar.k@indictanstech.com>
2016-01-15 16:39:51 +0530

    resolved the issue of update database master service

--- church_ministry/church_ministry/doctype/member/member.py
+++ church_ministry/church_ministry/doctype/member/member.py
@@ -992,7 +992,7 @@ def get_master_details(data):
 		"Zones":"zone_code,zone_name,region,contact_phone_no,contact_email_id,zonal_hq",
 		"Regions":"region_code,region_name,contact_phone_no,contact_email_id",
 		"Invitees and Contacts":"name,title,invitee_contact_name,sex,convert_invitee_contact_to_ft,date_of_convert,date_of_birth,age_group,invited_by,source_of_invitation,phone_1,email_id",
-		"First Timer":"name,ftv_name as`First Name`,ftv_name as`Last Name`,date_of_birth,marital_info,phone_1,email_id,address,office_address,image",
+		"First Timer":"name,ftv_name ,surname,date_of_birth,marital_info,phone_1,email_id,address,office_address,image",
 		"Partnership Record":"name,partnership_arms,ministry_year,is_member,member,date,cell,FORMAT(amount,2) as amount,giving_or_pledge,giving_type,type_of_pledge,instrument__no,bank_name,branch"
 	}
 	tablename=dts['tbl']
